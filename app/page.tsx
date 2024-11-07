c192a4e93f705db4c13bead07ab1e65b3cbb62d4
bulish <babickovalibuse@seznam.cz>
2024-11-07 17:17:31 +0100

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-// app/page.js nebo app/page.tsx
-
 'use client'
 
+import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 
-export default function Home() {
+export function Home() {
+    // Použití useSearchParams pro načtení parametrů z URL
     const searchParams = useSearchParams();
     const name = searchParams.get('name') || "Unknown User";
     const eventName = searchParams.get('eventName') || "Unknown Event";
@@ -13,23 +13,30 @@ export default function Home() {
     return (
         <div className="container">
             <h1 className="title">Event<span>Hub</span></h1>
-            <h2 className="subtitle">You&apos;re Invited!</h2>
+            <h2 className="subtitle">You're Invited!</h2>
             
             <div className="infoBox">
                 <p className="label">👤 Name:</p>
-                <p className="value">{name || "Unknown User"}</p>
+                <p className="value">{name}</p>
             </div>
 
             <div className="infoBox">
                 <p className="label">🎉 Event:</p>
-                <p className="value">{eventName || "Unknown Event"}</p>
+                <p className="value">{eventName}</p>
             </div>
 
             <div className="infoBox">
                 <p className="label">📅{" "}Date:</p>
-                <p className="value">{date || "To Be Announced"}</p>
+                <p className="value">{date}</p>
             </div>
-
         </div>
     );
+}
+
+export default function PageWrapper() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Home />
+    </Suspense>
+  );
 }
